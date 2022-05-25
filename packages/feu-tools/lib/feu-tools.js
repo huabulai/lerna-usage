e610d290530e9b2d87eaf3f1a9c33ff2a23ffb43
liangwj <wj_kyu@163.com>
2022-05-26 00:43:32 +0800

    tools库升级4

--- packages/feu-tools/lib/feu-tools.js
+++ packages/feu-tools/lib/feu-tools.js
@@ -1,6 +1,6 @@
 function add(a, b) {
-	console.log("tools库升级3，调用函数add,入参：%d, %d",a ,b)
-    return (a + b)*3;
+	console.log("tools库升级4，调用函数add,入参：%d, %d",a ,b)
+    return (a + b)*4;
 }
 function min(a, b) {
 	console.log("tools库，调用函数min,入参：%d, %d",a ,b)
