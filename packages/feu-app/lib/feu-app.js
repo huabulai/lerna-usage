5c3f664d1b5c318112c8bfee8c415f76fe90ebf8
liangwj <wj_kyu@163.com>
2022-05-26 11:32:38 +0800

    app库升级

--- packages/feu-app/lib/feu-app.js
+++ packages/feu-app/lib/feu-app.js
@@ -2,9 +2,11 @@ const { min } = require('feu-tools');
 const ui = require("feu-ui");
 
 function app() {
-    console.log("app主入口");
+    console.log("app主入口升级");
     let minNum = min(2, 5);
     ui(minNum);
 }
 
 app();
+
+module.exports = app;
